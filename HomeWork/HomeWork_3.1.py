197fefdda508584c5558b3a1c54ec513a224104d
olha-d-d-00 <olgadema39@dmail.com>
2025-05-17 00:38:39 +0300

    Homework Lesson 3 (3.1, 3.2)

---
+++ HomeWork/HomeWork_3.1.py
@@ -0,0 +1,19 @@
+number_1 = int(input("Enter the first number: "))
+number_2 = int(input("Enter the second number: "))
+
+
+print("There are such mathematical operations:\n\t1. Addition (+)\n\t2. Subtraction (-)\n\t3. Multiplication (*)\n\t4. Division (/)")
+user_select = int(input("Select the required operation: "))
+
+match user_select:
+    case 1:
+        print(number_1 + number_2)
+    case 2:
+        print(number_1 - number_2)
+    case 3:
+        print(number_1 * number_2)
+    case 4:
+        if number_2 == 0:
+            print("You can't divide by 0!")
+        else:
+            print(number_1 / number_2)
---
+++ HomeWork/HomeWork_3.2.py
@@ -0,0 +1,4 @@
+list = [12, 3, 4, 10]
+list.insert(0, 10)
+list.pop()
+print(list)
