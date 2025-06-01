78d51c5f987ed7b8e6afb08a1ef5329e9f098883
olha-d-d-00 <olgadema39@dmail.com>
2025-06-01 21:56:33 +0300

    HomeWork 6.3

--- HomeWork/HomeWork_6.3.py
+++ HomeWork/HomeWork_6.3.py
@@ -1,15 +1,16 @@
 user_number = input("Enter your number: ")
 _number = [int(i) for i in user_number]
 
+if len(_number) == 1:
+    print(_number[0])
+else:
+    while len(_number) > 1:
+        result = 1
+        for d in _number:
+            result *= d
+        if result <= 9:
+            break
 
-while len(_number) > 1:
-    result = 1
-    for d in _number:
-        result *= d
-    if result <= 9:
-        break
+        _number = [int(i) for i in str(result)]
 
-    _number = [int(i) for i in str(result)]
-
-
-print(result)
+    print(result)
