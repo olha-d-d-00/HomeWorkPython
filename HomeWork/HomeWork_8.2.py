4e134a78190623ca65c8271cdd55223a59064049
olha-d-d-00 <olgadema39@dmail.com>
2025-06-11 20:04:06 +0300

    howework 8

--- HomeWork/HomeWork_8.2.py
+++ HomeWork/HomeWork_8.2.py
@@ -2,7 +2,6 @@ def is_palindrome(text):
     word_user = "".join(i.lower() for i in text if i.isalnum())
     return word_user == word_user[::-1]
 
-     
 assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1' 
 assert is_palindrome('0P') == False, 'Test2' 
 assert is_palindrome('a.') == True, 'Test3' 
